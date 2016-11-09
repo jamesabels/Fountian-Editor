1f551872e2c131177d404cf506302a83f8c890cc
James Abels <jamesrabels@gmail.com>
2016-11-08 23:16:33 -0500

    New layout with scene section

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,34 +1,33 @@
 <template>
-  <div id="appHeader">
-    <header class="toolbar toolbar-header">
-      <div class="toolbar-actions">
-        <div class="btn-group">
-          <button class="btn btn-default">
+  <header id="header" class="toolbar toolbar-header col-xs-12">
+    <div class="toolbar-actions">
+      <div class="btn-group">
+        <button class="btn btn-default">
         <span class="icon icon-home"></span>
       </button>
-          <button class="btn btn-default">
+        <button class="btn btn-default">
         <span class="icon icon-folder"></span>
       </button>
-          <button class="btn btn-default active">
+        <button class="btn btn-default active">
         <span class="icon icon-cloud"></span>
       </button>
-          <button class="btn btn-default">
+        <button class="btn btn-default">
         <span class="icon icon-popup"></span>
       </button>
-          <button class="btn btn-default">
+        <button class="btn btn-default">
         <span class="icon icon-shuffle"></span>
       </button>
-        </div>
-        <button class="btn btn-default">
+      </div>
+      <button class="btn btn-default">
       <span class="icon icon-eye" v-on:click="previewFountain"></span>
     </button>
-        <button class="btn btn-default btn-dropdown pull-right">
+      <button class="btn btn-default btn-dropdown pull-right">
       <span class="icon icon-megaphone"></span>
     </button>
-      </div>
-    </header>
-  </div>
+    </div>
+  </header>
 </template>
+
 <script lang="babel">
 import Store from '../store/store.js'
 
